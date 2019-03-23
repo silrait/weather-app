2c1a2b51c75249472d243f1c7bce3e0bfb2e87d1
silrait <thiarllisba@hotmail.com>
2019-03-23 16:10:14 -0300

    Destructuring e Shorthand property

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ yargs.command({
         }
     },
     handler(argv){
-        geocode(argv.location, (error, data) => {
+        geocode(argv.location, (error, {latitute, longitude, location}) => {
             if(error){
                 return console.log(error)
             }
         
-            forecast(data.latitute, data.longitude, (error, forecastData) => {
+            forecast(latitute, longitude, (error, forecastData) => {
                 if(error){
                     return console.log(error)                
                 }
                 
-                console.log(data.location)               
+                console.log(location)               
                 console.log(forecastData)
             })
         })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,17 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {    
-    const urlDarkSky = `https://api.darksky.net/forecast/760e6309c79ea439e20ba205eca00796/${latitude},${longitude}?units=si`
+    const url = `https://api.darksky.net/forecast/760e6309c79ea439e20ba205eca00796/${latitude},${longitude}?units=si`
     
-    request({ url: urlDarkSky, json: true }, (error, response) => {    
+    request({ url, json: true }, (error, {body}) => {    
         if(error){
             callback('Unable to connect to weather service', undefined)
-        }else if(response.body.error) {
+        }else if(body.error) {
             callback('Unable to find location', undefined)
-        } else{ 
-            const summary = response.body.daily.data[0].summary
-            const temperature = response.body.currently.temperature
-            const precipProbability =  response.body.currently.precipProbability
+        } else{
+            const summary = body.daily.data[0].summary
+            const {temperature, precipProbability} = body.currently
             callback(undefined, `${summary} It is ${temperature} degrees out. There is a ${precipProbability}% of rain today.`)
         }
     })
